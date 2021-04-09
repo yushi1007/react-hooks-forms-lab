80dfb25c98b3dbe256ca945f9aa494c93d9c719d
Yu Shi <yushiys95@gmail.com>
2021-04-09 00:17:39 -0400

    commit tree e0add6c60473e3f5342f3fa378ee7e8d9cead2fd

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange }) {
+
+  function handleSearchChange() {
+    // we could pass up the whole event object via onSearchChange
+    // but passing *only* the value makes the data easier to work with in the parent
+    onSearchChange();
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input onChange={handleSearchChange} type="text" name="search" placeholder="Search..." />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
